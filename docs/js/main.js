835f7aaa878d521d1d75cb8c75adf23d012ec4e8
alex_crive <alexcrive95@yandex.ru>
2019-08-28 21:02:08 +0300

    first commit

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ $('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 100,
+    arrows: false,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    centerPadding: '0px'
+ })
+});
